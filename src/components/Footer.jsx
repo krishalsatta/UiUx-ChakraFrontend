70f2ef775ac618843414e2ca57672d00e319ca9f
krishalsatta <sattakrishal97@gmail.com>
2025-01-26 12:49:39 +0545

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Footer.css'; // Import the Footer specific CSS
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.css"; // Import the Footer specific CSS
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="waves">
+      {/* <div className="waves">
         <div className="wave" id="wave1"></div>
         <div className="wave" id="wave2"></div>
         <div className="wave" id="wave3"></div>
         <div className="wave" id="wave4"></div>
-      </div>
+      </div> */}
 
       {/* <ul className="social-icon">
         <li className="social-icon__item">
@@ -36,14 +36,30 @@ const Footer = () => {
       </ul> */}
 
       <ul className="menu">
-        <li className="menu__item"><Link to="/" className="menu__link">Home</Link></li>
-        <li className="menu__item"><Link to="/aboutus" className="menu__link">About</Link></li>
-        <li className="menu__item"><Link to="/services" className="menu__link">Services</Link></li>
-        <li className="menu__item"><Link to="/team" className="menu__link">Team</Link></li>
-        <li className="menu__item"><Link to="/contact" className="menu__link">Contact</Link></li>
+        <li className="menu__item">
+          <Link to="/" className="menu__link">
+            Home
+          </Link>
+        </li>
+        <li className="menu__item">
+          <Link to="/aboutus" className="menu__link">
+            About
+          </Link>
+        </li>
+        <li className="menu__item">
+          <Link to="/services" className="menu__link">
+            Services
+          </Link>
+        </li>
+        {/* <li className="menu__item"><Link to="/team" className="menu__link">Team</Link></li> */}
+        <li className="menu__item">
+          <Link to="/contact" className="menu__link">
+            Contact
+          </Link>
+        </li>
       </ul>
 
-      <p>&copy;2024 Too Ease. All Rights Reserved.</p>
+      <p>&copy;2024 CHAKRA. All Rights Reserved.</p>
     </footer>
   );
 };
